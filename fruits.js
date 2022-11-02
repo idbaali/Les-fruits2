c3ade2dbb55c64ac398f83bd1251a9fea9eab870
idbaali <ikebaalidavid@live.de>
2022-11-02 14:18:17 +0100

    Les fruits2

---
+++ fruits.js
@@ -0,0 +1,19 @@
+let fruits = [{ name: "Orange", image: "orange.jpg" }, { name: "Kiwi", image: "kiwi.jpg" }, { name: "Banane", image: "banane.jpg" }, { name: "Peche", image: "peche.jpg" }, { name: "Cerise", image: "cerise.jpg" }];
+
+const imageBlock = document.querySelector("#imageFruit");
+        const buttonBlock =  document.querySelector("#fruits");
+        fruits.forEach(fruit => {
+        const divElement = document.createElement("div");
+        divElement.textContent = fruit.name;
+        buttonBlock.appendChild(divElement)
+        divElement.className = "boutonFruits";
+        divElement.style.height = 100 / fruits.length+"vh";
+        divElement.addEventListener("mouseover", (event) => {
+            imageBlock.style.backgroundImage = "url("+ fruit.image +")";
+        })
+        divElement.addEventListener("mouseout", (event) => {
+            imageBlock.style.backgroundImage = "url(all-fruits.jpg)";
+        })   
+})
+
+
